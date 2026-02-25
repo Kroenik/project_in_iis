43c01ef1d7637f892cd62068cda758ebf4df59f1
kroenik <niklas_kr@hotmail.de>
2026-02-25 01:39:38 +0100

    ui fix

--- frontend.py
+++ frontend.py
@@ -49,7 +49,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Talk to me to find volunteering opportunities!"):
     agent_input = {"messages": [{"role": "user", "content": prompt}]}
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
